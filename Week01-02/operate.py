3aa5566f5d8d927bff0cac40e907a6418ceebfe1
Blayke McGregor <bamcg3@student.monash.edu>
2021-08-04 17:53:19 +1000

    Got operate.py working for Milestone 1

--- Week01-02/operate.py
+++ Week01-02/operate.py
@@ -137,15 +137,19 @@ class Operate:
             ############### add your codes below ###############
             # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                self.command['motion'] = [3, 0]
                 pass # TODO: replace with your code to make the robot drive forward
             # drive backward
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+                self.command['motion'] = [-3, 0]
                 pass # TODO: replace with your code to make the robot drive backward
             # turn left
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+                self.command['motion'] = [0, 2]
                 pass # TODO: replace with your code to make the robot turn left
             # drive right
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                self.command['motion'] = [0, -2]
                 pass # TODO: replace with your code to make the robot turn right
             ####################################################
             # stop
